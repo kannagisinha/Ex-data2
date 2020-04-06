d1eb1700e95cb440b5684782bd5d6e8a25224f68
kannagisinha <61078441+kannagisinha@users.noreply.github.com>
2020-04-06 19:17:43 +0530

    Create plot5.R

---
+++ plot5.R
@@ -0,0 +1,19 @@
+library("data.table")
+library("ggplot2")
+
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project2")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+NEI <- data.table::as.data.table(x = readRDS("summarySCC_PM25.rds"))
+SCC <- data.table::as.data.table(x = readRDS("Source_Classification_Code.rds"))
+vehiclesSCC <- SCC[grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+                   , SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+baltimoreVehiclesNEI <- vehiclesNEI[fips=="24510",]
+png("plot5.png")
+ggplot(baltimoreVehiclesNEI,aes(factor(year),Emissions)) +
+  geom_bar(stat="identity", fill ="#FF9999" ,width=0.75) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
